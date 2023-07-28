205c0d29c82ce54f20c249b5277484b71ff4b6d8
Kevin Garcia <garciakevin0084@outlook.es>
2023-07-27 21:27:44 -0500

    section 6

---
+++ src/app/dbz/dbz.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MainPageComponent } from './pages/main-page.componet';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
+
+
+@NgModule({
+  declarations: [
+    AddCharacterComponent,
+    MainPageComponent,
+    ListComponent,
+  ],
+  exports: [
+    MainPageComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class DbzModule { }
